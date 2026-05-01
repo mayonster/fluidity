fbf63ccc1fecc2defc1d86d25fa39c4893f33591
mayonster <mayonster@mayonster.com>
2026-05-01 03:17:44 -0400

    Added files for linked lists but did not populate

---
+++ listFuncs.c
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+#include "listFuncs.h"
+
+void init_list 
---
+++ listFuncs.h
@@ -0,0 +1,9 @@
+#ifndef LISTFUNCS_H
+#define LISTFUNCS_H
+
+typedef struct {
+	struct list *next;
+	struct list *prev;
+} List;
+
+#endif
